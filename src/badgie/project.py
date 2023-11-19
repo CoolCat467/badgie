6f0489f30d1796f58fb255c3b66b322762a2b6eb
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2023-11-19 06:37:33 -0600

    Move to src layout and fix tests

--- src/badgie/project.py
+++ src/badgie/project.py
@@ -10,7 +10,7 @@ from typing import TYPE_CHECKING
 from badgie.models import ProjectRemote
 
 if TYPE_CHECKING:
-    from collections.abc import Final
+    from typing import Final
 
 REMOTES: Final = (
     r"(?P<user>git)@(?P<host>.*?):(?P<path>.*?)\.git",
